feeaad7e63a039aa9610eb1f47414a80495ea811
Ramkumar Velmurugan <vrk1990@outlook.com>
2023-05-16 21:00:42 -0400

    project init :rocket:

---
+++ app/src/main/java/com/spkd/bbcradioplayer/MainActivity.kt
@@ -0,0 +1,46 @@
+package com.spkd.bbcradioplayer
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.spkd.bbcradioplayer.ui.theme.BBCRadioPlayerTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            BBCRadioPlayerTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    BBCRadioPlayerTheme {
+        Greeting("Android")
+    }
+}
